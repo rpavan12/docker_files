073d2ca4c219cfa4534000a6d44048eeb772115c
pavan <pavan@gmail.com>
2025-08-02 21:06:26 +0530

    terraform file added

--- server_creation/ec2.tf
+++ server_creation/ec2.tf
@@ -3,9 +3,30 @@ provider "aws" {
   
 }
 
-resource "aws_instance" "docker {
-  ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID, replace with a valid one
+resource aws_security_group "docker_sg" {
+  name        = "docker_security_group"
+  description = "Security group for Docker instance"
+  
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "docker" {
+  ami           = "ami-0c614dee691cbbf37" 
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.docker_sg.name]
+  
   
 #we need large disk size to install docker so we are using below
 root_block_device {
